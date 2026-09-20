f3e24cf0375e6239bf56de85ec401e8a1e72d35f
sandesh subedi <152250495+sanmarshall@users.noreply.github.com>
2026-09-20 20:55:22 +0545

    Add routes for student, admin, and teacher

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 import config from "./config/config.js";
 import connectDb from "./config/database.js";
 import express from "express";
-
+import studentRoute from "./routes/student.route.js";
+import adminRoute from "./routes/admin.route.js";
+import teacherRoute from "./routes/teacher.route.js";
 import cors from "cors";
 
 const app = express();
@@ -12,11 +14,14 @@ connectDb();
 
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Welcome to the API" });
 });
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/student", studentRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/teacher", teacherRoute);
 
 
 app.listen(config.port, () => {
